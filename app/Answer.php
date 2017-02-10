e43a8bce26cfbe1d503fa9320b640486890489fd
magamlag <lagovskiy@gmail.com>
2017-02-10 17:25:25 -0500

    Build relations between Answer and Questions models

--- app/Answer.php
+++ app/Answer.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Answer extends Model
-{
-    //
+class Answer extends Model {
+	//
+	protected $guarded = [];
+
+	/**
+	 * Every answer can have only one question
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function question() {
+		return $this->belongsToMany( Question::class );
+	}
 }
--- app/Question.php
+++ app/Question.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+	protected $guarded = [];
+
+	/**
+	 * Every 'question' can have many 'answers'
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function answers(  ) {
+		return $this->hasMany( Answer::class );
+	}
 }
