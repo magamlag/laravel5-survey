8c0e3363873ce1937b5af6f8d3fdba486a073c6b
magamlag <lagovskiy@gmail.com>
2017-02-17 14:39:04 -0500

    Call ParticipantsAnswers seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
 			$this->call(UsersTableSeeder::class);
       $this->call(QuestionsTableSeeder::class);
       $this->call(AnswersTableSeeder::class);
+      $this->call(ParticipantsAnswersTableSeeder::class);
 
       Model::reguard();
     }
