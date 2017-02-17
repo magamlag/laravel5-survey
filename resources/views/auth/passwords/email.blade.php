c0787e50b1e07ddddd0776c158f795de3eae24ea
magamlag <lagovskiy@gmail.com>
2017-02-17 15:51:54 -0500

    Rename link

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Отправить ссылку на почту
+                                    Send link on email
                                 </button>
                             </div>
                         </div>
