4cc3af2a99f19efc75befe717688188865d9c207
magamlag <lagovskiy@gmail.com>
2017-02-22 19:44:45 -0500

    Install and Setup Codeception

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,22 +28,17 @@ class HomeController extends Controller
     public function index()
     {
         if(Auth::user()->status == 0) {
-            $q = Question::all();
+            $q = Question::with('answers')->has('answers')->take(10)->get();
             $questions = [];
-
             foreach ($q as $item) {
-                $a = Answer::where('qId', $item->id)->get()->toArray();
 
                 $questions[$item->id]['id'] = $item->id;
                 $questions[$item->id]['title'] = $item->text;
 
-                foreach ($a as $k => $answer) {
-                    $questions[$item->id]['a' . $k] = $answer['text'];
-                    $questions[$item->id]['a' . $k . 'id'] = $answer['id'];
+                foreach ($item->answers as $k => $answer) {
+                    $questions[$item->id]['answers'][$answer['id']] = $answer['text'];
                 }
             }
-
-
             $data = ['questions' => $questions];
         } else {
 
@@ -57,26 +52,10 @@ class HomeController extends Controller
     }
 
     public function addAnsw(Request $request){
-        $a1 = Answer::where('id', $request->cg1)->get()->toArray()[0]['count'];
-        $a2 = Answer::where('id', $request->cg2)->get()->toArray()[0]['count'];
-        $a3 = Answer::where('id', $request->cg3)->get()->toArray()[0]['count'];
+			$request_array = $request->except('_token');
 
-        Answer::where('id', $request->cg2)->update([
-            'count' => $a2 + 1
-        ]);
+				Auth::user()->answers()->sync(array_values($request_array));
 
-        Answer::where('id', $request->cg1)->update([
-            'count' => $a1 + 1
-        ]);
-
-        Answer::where('id', $request->cg3)->update([
-            'count' => $a3 + 1
-        ]);
-
-        User::where('id', Auth::user()->id)->update([
-            'status'=>1
-        ]);
-
-        return redirect()->route('home');
+				return redirect()->route('home');
     }
 }
