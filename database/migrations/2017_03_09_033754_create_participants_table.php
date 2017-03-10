9208a4f1d114e3b465de60efa39aa6667903ef47
magamlag <lagovskiy@gmail.com>
2017-03-09 22:30:47 -0500

    Create migration table for pivot table - 'participants'

---
+++ database/migrations/2017_03_09_033754_create_participants_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            //
+						$table->increments('id');
+						$table->integer('answer_id')->unsigned();
+						$table->integer('user_id')->unsigned();
+						$table->integer('question_id')->unsigned();
+						$table->foreign('answer_id')
+							->references('id')->on('answers')
+							->onDelete('cascade');
+						$table->foreign('question_id')
+								->references('id')->on('questions')
+								->onDelete('cascade');
+						$table->foreign('user_id')
+								->references('id')->on('users')
+								->onDelete('cascade');
+						$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            //
+        });
+    }
+}
